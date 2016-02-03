b5bd887aa9f0beb07973ae37c8ce1ef212cd4ebc
Christoph Herbst <chris.p.herbst@gmail.com>
2016-02-03 13:01:19 +0100

    change filter name to match badgeos-community-addon

--- includes/rules-engine.php
+++ includes/rules-engine.php
@@ -3,7 +3,7 @@ add_action( 'init',  function() {
 	// if badgeos_community does not exist, bail
 	if (!isset($GLOBALS['badgeos_community']))
 		return;
-	
+
     $GLOBALS['badgeos_community']->community_triggers[__( 'Social Actions', 'badgeos-community' )]
         ['badgeos_bp_follow_start_following'] = __( 'Follow another user', 'badgeos-community-follow' );
     $GLOBALS['badgeos_community']->community_triggers[__( 'Social Actions', 'badgeos-community' )]
@@ -15,7 +15,7 @@ add_action( 'bp_follow_start_following',  function($follow) {
     do_action('badgeos_bp_follow_new_follower', $follow->leader_id);
 });
 
-add_filter('badgeos_bp_trigger_user_id', function($user_ID, $current_filter, $args ){
+add_filter('badgeos_bp_trigger_event_user_id', function($user_ID, $current_filter, $args ){
     if ( 'badgeos_bp_follow_new_follower' == $current_filter )
 		return absint( $args );
 	return $user_ID;
